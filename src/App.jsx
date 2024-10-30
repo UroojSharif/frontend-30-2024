99d7f42113c1796ba8e9a682dfe26099aaf56ca7
urooj <urooj.shareef@hotmail.com>
2024-10-30 11:47:35 +0100

    frontend-deploy1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+
+function App() {
+  return (
+    <>
+      <h1>User registration</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Form() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [details, setDetails] = useState([]);
+
+  const userData = { name, email, password }; //we send this object to backend
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/submit/",
+        userData
+      );
+      // console.log(response.data.details);
+
+      setDetails(response.data.details);
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div>
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </label>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <label>
+          Email:
+          <input
+            type="text"
+            name="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        &nbsp;&nbsp;
+        <label>
+          Password:
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <br />
+        <input type="submit" value="Submit" onClick={handleSubmit} />
+      </form>
+      {details.map((user) => {
+        return (
+          <div key={user._id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Form;
